55b11db23a70c38068800bb098ddb4f0637e6f1b
Staff <staff@iMac-2.local>
2018-05-31 14:55:27 +0100

    added a get() command in

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const express = require('express');
 
 const app = express();
 
+// Index route
+app.get(); 
+
 const port = 5000;
 
 app.listen( port, () => console.log(`Server started on port ${port}`));
